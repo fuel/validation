d119953eea522bb1e4192ad6369614ecf54cc709
Steve "Uru" West <uruwolf@gmail.com>
2013-11-07 10:20:48 +0000

    Adds valid URL rule

---
+++ src/Fuel/Validation/Rule/ValidUrl.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @package   Fuel\Validation
+ * @version   2.0
+ * @author    Fuel Development Team
+ * @license   MIT License
+ * @copyright 2010 - 2013 Fuel Development Team
+ * @link      http://fuelphp.com
+ */
+
+namespace Fuel\Validation\Rule;
+
+use Fuel\Validation\AbstractRule;
+
+/**
+ * Performs validation to check that a value is a valid url or not
+ *
+ * @package Fuel\Validation\Rule
+ * @author  Fuel Development Team
+ * @since   2.0
+ */
+class ValidUrl extends AbstractRule
+{
+
+	/**
+	 * @param mixed  $params
+	 * @param string $message
+	 *
+	 * @since 2.0
+	 */
+	public function __construct($params = null, $message = '')
+	{
+		if (empty($message))
+		{
+			$message = 'The field is not a valid IP address.';
+		}
+
+		parent::__construct($params, $message);
+	}
+
+	/**
+	 * @param mixed  $value     Value to validate
+	 * @param string $field     Unused by this rule
+	 * @param array  $allFields Unused by this rule
+	 *
+	 * @return bool
+	 *
+	 * @since 2.0
+	 */
+	public function validate($value, $field = null, &$allFields = null)
+	{
+		return false !== filter_var($value, FILTER_VALIDATE_URL);
+	}
+}
---
+++ tests/Fuel/Validation/Rule/ValidUrlTest.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * @package   Fuel\Validation
+ * @version   2.0
+ * @author    Fuel Development Team
+ * @license   MIT License
+ * @copyright 2010 - 2013 Fuel Development Team
+ * @link      http://fuelphp.com
+ */
+
+namespace Fuel\Validation\Rule;
+
+/**
+ * Tests for ValidUrl
+ *
+ * @package Fuel\Validation\Rule
+ * @author  Fuel Development Team
+ */
+class ValidUrlTest extends \PHPUnit_Framework_TestCase
+{
+
+	/**
+	 * @var ValidUrl
+	 */
+	protected $object;
+
+	protected function setUp()
+	{
+		$this->object = new ValidUrl;
+	}
+
+	/**
+	 * @coversDefaultClass getMessage
+	 * @group              Validation
+	 */
+	public function testGetMessage()
+	{
+		$this->assertEquals(
+			 'The field is not a valid IP address.',
+			 $this->object->getMessage()
+		);
+	}
+
+	/**
+	 * @coversDefaultClass validate
+	 * @dataProvider       validateProvider
+	 * @group              Validation
+	 */
+	public function testValidate($url, $expected)
+	{
+		$this->assertEquals(
+			$expected,
+			$this->object->validate($url)
+		);
+	}
+
+	/**
+	 * Provides strings to test and expected results for testValidate
+	 *
+	 * @return array
+	 */
+	public function validateProvider()
+	{
+		return array(
+			array('', false),
+			array(1, false),
+			array(true, false),
+			array(new \stdClass, false),
+			array('512.123.1254.34234', false),
+			array('http://fuelphp.com', true),
+			array('http://fuelphp', true),
+			array('fuelphp.com', false),
+			array('sftp://user:password@fuelphp.com', true),
+			array('http://192.168.0.1', true),
+			array('ftp://FE80::0202:B3FF:FE1E:8329', true),
+		);
+	}
+
+	/**
+	 * @coversDefaultClass __construct
+	 * @coversDefaultClass getMessage
+	 * @group              Validation
+	 */
+	public function testCustomMessageOnConstruct()
+	{
+		$message = 'foobar';
+
+		$object = new ValidUrl(null, $message);
+
+		$this->assertEquals(
+			$message,
+			$object->getMessage()
+		);
+	}
+
+}
