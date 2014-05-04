36848df11b4d6b19d51b5f97624130049859f4c6
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-05-04 18:27:40 +0200

    Improves Regex rule coverage

--- tests/Fuel/Validation/Rule/RegexTest.php
+++ tests/Fuel/Validation/Rule/RegexTest.php
@@ -66,4 +66,20 @@ class RegexTest extends AbstractRuleTest
 		);
 	}
 
+	/**
+	 * @covers ::setParameter
+	 * @group  Validation
+	 */
+	public function testSetParameter()
+	{
+		$parameter = array(12);
+
+		$this->object->setParameter($parameter);
+
+		$this->assertEquals(
+			12,
+			$this->object->getParameter()
+		);
+	}
+
 }
